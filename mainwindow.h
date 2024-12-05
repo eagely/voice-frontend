00da35ab8101e729fe00005e99c9a7f72d7c0777
Artemiy Smirnov <artemiy.smirnov@htlstp.at>
2024-12-05 14:44:38 +0100

    Implement weather requests

--- mainwindow.h
+++ mainwindow.h
@@ -1,23 +1,29 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QAudioInput>
 #include <QMainWindow>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
 
-QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
 }
-QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void onButtonClicked();
+    void handleNetworkReply(QNetworkReply *reply);
 
 private:
     Ui::MainWindow *ui;
+    QNetworkAccessManager *networkManager;
 };
+
 #endif // MAINWINDOW_H
