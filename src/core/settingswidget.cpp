bdaea9819b9e970ad94b9ae9087c410d9fbd55a9
Artemiy Smirnov <artemiy.smirnov@proton.me>
2025-03-08 22:07:42 +0100

    Add parsing, general answers and output config buttons

--- src/core/settingswidget.cpp
+++ src/core/settingswidget.cpp
@@ -1,5 +1,6 @@
 #include "settingswidget.h"
 #include <QButtonGroup>
+#include <QDebug>
 
 SettingsWidget::SettingsWidget(QWidget *parent) :
     QWidget(parent),
@@ -8,19 +9,33 @@ SettingsWidget::SettingsWidget(QWidget *parent) :
     ui->setupUi(this);
     settings = new QSettings("eagely", "voice");
 
-    audioRecordingGroup = new QButtonGroup();
-    textToSpeechGroup = new QButtonGroup();
+    audioRecordingGroup = new QButtonGroup(this);
+    speechToTextGroup = new QButtonGroup(this);
+    generalAnswersGroup = new QButtonGroup(this);
+    parsingGroup = new QButtonGroup(this);
+    outputGroup = new QButtonGroup(this);
 
     audioRecordingGroup->addButton(ui->recorderLocalButton);
     audioRecordingGroup->addButton(ui->recorderRemoteButton);
 
-    textToSpeechGroup->addButton(ui->transcriberLocalWhisperButton);
-    textToSpeechGroup->addButton(ui->trancriberOpenAIApiWhisperButton);
-    textToSpeechGroup->addButton(ui->transcriberRemoteWhisperButton);
+    speechToTextGroup->addButton(ui->speechToTextLocalButton);
+    speechToTextGroup->addButton(ui->speechToTextCloudButton);
+
+    generalAnswersGroup->addButton(ui->generalAnswersLocalButton);
+    generalAnswersGroup->addButton(ui->generalAnswersCloudButton);
+
+    parsingGroup->addButton(ui->parsingSimpleButton);
+    parsingGroup->addButton(ui->parsingAdvancedButton);
+
+    outputGroup->addButton(ui->outputAudioButton);
+    outputGroup->addButton(ui->outputTextButton);
 
     connect(ui->backButton, &QPushButton::clicked, this, &SettingsWidget::openMainWidget);
-    connect(audioRecordingGroup, &QButtonGroup::idClicked, this, &SettingsWidget::onAudioRecording);
-    connect(textToSpeechGroup, &QButtonGroup::idClicked, this, &SettingsWidget::onTextToSpeech);
+    connect(audioRecordingGroup, QOverload<int>::of(&QButtonGroup::idClicked), this, &SettingsWidget::onAudioRecordingGroupPressed);
+    connect(speechToTextGroup, QOverload<int>::of(&QButtonGroup::idClicked), this, &SettingsWidget::onSpeechToTextGroupPressed);
+    connect(generalAnswersGroup, QOverload<int>::of(&QButtonGroup::idClicked), this, &SettingsWidget::onGeneralAnswersGroupPressed);
+    connect(parsingGroup, QOverload<int>::of(&QButtonGroup::idClicked), this, &SettingsWidget::onParsingGroupPressed);
+    connect(outputGroup, QOverload<int>::of(&QButtonGroup::idClicked), this, &SettingsWidget::onOutputGroupPressed);
 }
 
 SettingsWidget::~SettingsWidget()
@@ -28,20 +43,42 @@ SettingsWidget::~SettingsWidget()
     delete ui;
 }
 
-void SettingsWidget::onAudioRecording(qint32 id) {
+void SettingsWidget::onAudioRecordingGroupPressed(qint32 id) {
     if (id == audioRecordingGroup->id(ui->recorderLocalButton)) {
-        qDebug() << "Audio Recording selected: Local Recorder";
+        qDebug() << "Audio Recording selected: Local";
     } else if (id == audioRecordingGroup->id(ui->recorderRemoteButton)) {
-        qDebug() << "Audio Recording selected: Remote Recorder";
+        qDebug() << "Audio Recording selected: Remote";
     }
 }
 
-void SettingsWidget::onTextToSpeech(qint32 id) {
-    if (id == textToSpeechGroup->id(ui->transcriberLocalWhisperButton)) {
-        qDebug() << "Text-To-Speech selected: Local Whisper";
-    } else if (id == textToSpeechGroup->id(ui->trancriberOpenAIApiWhisperButton)) {
-        qDebug() << "Text-To-Speech selected: OpenAI API Whisper";
-    } else if (id == textToSpeechGroup->id(ui->transcriberRemoteWhisperButton)) {
-        qDebug() << "Text-To-Speech selected: Remote Whisper";
+void SettingsWidget::onSpeechToTextGroupPressed(qint32 id) {
+    if (id == speechToTextGroup->id(ui->speechToTextLocalButton)) {
+        qDebug() << "Speech-To-Text selected: Local";
+    } else if (id == speechToTextGroup->id(ui->speechToTextCloudButton)) {
+        qDebug() << "Speech-To-Text selected: Cloud";
+    }
+}
+
+void SettingsWidget::onGeneralAnswersGroupPressed(qint32 id) {
+    if (id == generalAnswersGroup->id(ui->generalAnswersLocalButton)) {
+        qDebug() << "General Answers selected: Local";
+    } else if (id == generalAnswersGroup->id(ui->generalAnswersCloudButton)) {
+        qDebug() << "General Answers selected: Cloud";
+    }
+}
+
+void SettingsWidget::onParsingGroupPressed(qint32 id) {
+    if (id == parsingGroup->id(ui->parsingSimpleButton)) {
+        qDebug() << "Parsing selected: Simple";
+    } else if (id == parsingGroup->id(ui->parsingAdvancedButton)) {
+        qDebug() << "Parsing selected: Advanced";
+    }
+}
+
+void SettingsWidget::onOutputGroupPressed(qint32 id) {
+    if (id == outputGroup->id(ui->outputAudioButton)) {
+        qDebug() << "Output selected: Audio";
+    } else if (id == outputGroup->id(ui->outputTextButton)) {
+        qDebug() << "Output selected: Text";
     }
 }
